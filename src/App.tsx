a361ec366cb84e9b451714170b545d386f45d20c
an288705 <aj0466012@gmail.com>
2023-10-22 22:57:33 -0700

    use mapbox instead of google maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import GoogleMapReact from "google-map-react";
+import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 import "./App.css";
 
 function App() {
@@ -11,11 +12,17 @@ function App() {
   return (
     <div className="App">
       <div style={{ height: "50vh", width: "50%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY || "" }}
-          defaultCenter={location}
-          defaultZoom={17}
-        ></GoogleMapReact>
+        <Map
+          mapboxAccessToken="pk.eyJ1IjoiYW4yODg3MDUiLCJhIjoiY2xvMmd3emlmMDdsZjJscGpmcHdrMTE5dyJ9.ecwGkQOZhDLyXtQJeSBsjw"
+          initialViewState={{
+            latitude: location.lat,
+            longitude: location.lng,
+            zoom: 15,
+          }}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+        >
+          <Marker latitude={location.lat} longitude={location.lng} />
+        </Map>
       </div>
     </div>
   );
