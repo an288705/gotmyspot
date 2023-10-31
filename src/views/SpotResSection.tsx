7a6ff897eb9cdf2ff2f236d0d00d6010cd7583bf
an288705 <aj0466012@gmail.com>
2023-10-31 00:01:40 -0700

    add search params

--- src/views/SpotResSection.tsx
+++ src/views/SpotResSection.tsx
@@ -1,13 +1,37 @@
 import React from "react";
+import { TextField } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker, TimePicker } from "@mui/x-date-pickers";
 import SpotCard from "./SpotCard";
 
 export default function SpotResSection() {
   const res = ["ex 1", "ex 2", "ex 3"];
   return (
-    <div>
-      {res.map((spotCard) => (
-        <SpotCard name={spotCard} />
-      ))}
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      Location
+      <div>
+        <TextField id="outlined-basic" variant="outlined" onChange={() => {}} />
+      </div>
+      Start
+      <div>
+        <DatePicker />
+      </div>
+      <div>
+        <TimePicker />
+      </div>
+      End
+      <div>
+        <DatePicker />
+      </div>
+      <div>
+        <TimePicker />
+      </div>
+      <div>
+        {res.map((spotCard) => (
+          <SpotCard name={spotCard} />
+        ))}
+      </div>
+    </LocalizationProvider>
   );
 }
