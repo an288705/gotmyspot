f2bfce3f3cfeab14ece9874c64b7da6a0002a5f1
an288705 <aj0466012@gmail.com>
2023-12-24 19:01:07 -0700

    revamp map render

--- src/views/customer/HomePage.tsx
+++ src/views/customer/HomePage.tsx
@@ -7,21 +7,19 @@ import { getSpotsByLatLong } from "../../controllers/apis";
 
 export default function HomePage() {
   const [spots, setSpots] = React.useState(Array<any>);
-  const [viewState, setViewState] = React.useState({
-    longitude: -122.08427,
-    latitude: 37.42216,
-    zoom: 15,
-  });
+  const [viewState, setViewState] = React.useState<any>();
 
   async function setSpotsState() {
-    const res = await getSpotsByLatLong(
-      viewState.latitude,
-      viewState.longitude,
-      0.01,
-    );
+    if (viewState) {
+      const res = await getSpotsByLatLong(
+        viewState.latitude,
+        viewState.longitude,
+        0.01,
+      );
 
-    console.log("res", res);
-    setSpots(res);
+      console.log("res", res);
+      setSpots(res);
+    }
   }
 
   React.useEffect(() => {
@@ -30,29 +28,33 @@ export default function HomePage() {
 
   return (
     <Grid container>
-      <Grid item style={{ height: "50vh", width: "50%" }}>
-        <Map
-          {...viewState}
-          id="mapData"
-          mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-          onMove={(e)=>setViewState({
-            longitude: e.viewState.longitude,
-            latitude: e.viewState.latitude,
-            zoom: 15,
-          })}
-          mapStyle="mapbox://styles/mapbox/streets-v11"
-        >
-          {spots.map((spot) => (
-            <Marker
-              latitude={spot.latitude}
-              longitude={spot.longitude}
-              onClick={() => {}}
-            >
-              <Paper>$20</Paper>
-            </Marker>
-          ))}
-        </Map>
-      </Grid>
+      {viewState && (
+        <Grid item style={{ height: "50vh", width: "50%" }}>
+          <Map
+            {...viewState}
+            id="mapData"
+            mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+            onMove={(e) =>
+              setViewState({
+                longitude: e.viewState.longitude,
+                latitude: e.viewState.latitude,
+                zoom: 15,
+              })
+            }
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+          >
+            {spots.map((spot) => (
+              <Marker
+                latitude={spot.latitude}
+                longitude={spot.longitude}
+                onClick={() => {}}
+              >
+                <Paper>$20</Paper>
+              </Marker>
+            ))}
+          </Map>
+        </Grid>
+      )}
       <Grid item>
         <SpotSearchSection setViewState={setViewState} />
         <SpotsReserveSection spots={spots} />
